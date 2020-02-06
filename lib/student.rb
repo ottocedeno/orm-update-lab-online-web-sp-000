7d54914d21fa427f2b556f06e7cc0b1d16089af2
Otto Cedeno <otto@ottostacos.com>
2020-02-06 15:36:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    self.new(row[1], row[2]).tap {|student| student.id = row[0]}
+    self.new(row[1], row[2]).tap {|student| @id = row[0]}
   end
 
 
